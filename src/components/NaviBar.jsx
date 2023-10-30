210bb5fd49472ea7339f783a075fada6b218299d
James Watson <jameywatson134@gmail.com>
2023-10-30 14:18:21 -0700

    react-icons are working

--- src/components/NaviBar.jsx
+++ src/components/NaviBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FaBars } from "react-icons/fa6";
 
 const NaviBar = () => {
 
-    const links = () => [
+    const links = [
         {
             id: 1,
             link: "Home"
@@ -23,24 +24,29 @@ const NaviBar = () => {
             id: 5,
             link: "Contact"
         },
-    ]
-    
+    ];
 
-
-    return <>
+    return (
         <div className="flex justify-between items-center w-full h-20 text-white bg-black fixed px-4">
-
             <div>
                 <h1 className="text-5xl font-navName ml-2">Watson</h1>
             </div>
-            <ul className='flex'>
-                <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'>Home</li>
-                <li className=''>Home</li>
+            <ul className='hidden md:flex'>
+                {links.map(({ id, link }) => (
+                    <li
+                        key={id}
+                        className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-110 duration-200'
+                    >
+                        {link}
+                    </li>
+                ))}
             </ul>
+
+            <div>
+                <FaBars/>
+            </div>
         </div>
-
-    </>
-
+    );
 };
 
 export default NaviBar;
