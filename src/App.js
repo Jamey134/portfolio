8bf6edfd74c8f64b651f552cdac5fc705d292f28
James Watson <jameywatson134@gmail.com>
2023-11-02 16:16:53 -0700

    "About" section updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import NaviBar from "./components/NaviBar";
 import Socials from "./components/Socials";
+import About from "./components/About"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NaviBar />
       <Home />
       <Socials />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const About = () => {
+
+    return (
+        <div name="about"
+            className='w-full h-screen bg-gradient-to-b from-gray-800 to-slate-700 text-white'
+        >
+            <div>
+                <div>
+                    <h2>About</h2>
+                </div>
+                <p>
+                I am a versatile full-stack developer with six months of hands-on experience. 
+                Before entering the field of software development, my background was in environmental science, which equipped me with valuable skills in research, creative problem-solving, and keen attention to detail.
+                </p>
+                <br/>
+                <p>
+                During my six months in software development, I've further honed these abilities and developed a strong passion for creating digital solutions that prioritize user needs. 
+                I thrive in the fast-paced and ever-changing tech landscape, always staying updated with the latest industry trends. 
+                I'm committed to delivering high-quality work and approach each project with a determination to achieve excellent results.
+                </p>
+                <br/>
+                <p>
+                I am eager to bring my unique perspective and dedication to your team, and I'm ready to contribute to innovative projects that have a meaningful impact. 
+                I look forward to the opportunity to connect and discuss how I can be a valuable asset to your organization.
+                </p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default About
