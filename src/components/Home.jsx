4272bae83d00ca524022cd50aa26a0dfd9de37ed
James Watson <jameywatson134@gmail.com>
2023-11-02 12:54:21 -0700

    button animation works

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row">
-                <div className="flex flex-col justify-center h-full">
+                <div className="flex flex-col justify-center h-full px-6 ">
                     <h2 className="text-4xl sm:text-7xl font-bold text-white">
                         I'm a Full Stack Developer
                     </h2>
@@ -28,7 +28,7 @@ const Home = () => {
                             duration={500}
                             className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
                          */}
-                        <button>
+                        <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-teal-500 to-blue-500 cursor-pointer">
                             Portfolio
                             <span className="group-hover:rotate-90 duration-300">
                                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
@@ -43,6 +43,7 @@ const Home = () => {
                         src={Hike}
                         alt="my profile"
                         className="rounded-2xl mx-auto w-2/3 md:w-full"
+                        style={{ width: 'auto', height: 'auto' }}
                     />
                 </div>
             </div>
